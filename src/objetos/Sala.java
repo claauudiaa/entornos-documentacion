4b161e409e8ccdd2b5164ca1eee8206bc91f0b3e
CLAUDIA CANDELAS OVIEDO <claudiacandelas2005@gmail.com>
2024-02-15 20:14:03 +0100

    (feat): Se añaden getter y setters en la clase Sala

--- src/objetos/Sala.java
+++ src/objetos/Sala.java
@@ -1,4 +1,49 @@
 package objetos;
 
+import java.util.Random;
+
 public class Sala {
+
+    Random rand = new Random();
+
+    private int numSala;
+    private Pelicula pelicula;
+    private int[][] butacas;
+
+    public Sala(int numSala, Pelicula pelicula, int filas, int columnas) {
+        this.numSala = numSala;
+        this.pelicula = pelicula;
+        this.butacas = new int[filas][columnas];
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                this.butacas[i][j] = rand.nextInt(2); // 0 representa libre
+            }
+        }
+    }
+
+    public int getNumSala() {
+        return numSala;
+    }
+
+    public void setNumSala(int numSala) {
+        this.numSala = numSala;
+    }
+
+    public Pelicula getPelicula() {
+        return pelicula;
+    }
+
+    public void setPelicula(Pelicula pelicula) {
+        this.pelicula = pelicula;
+    }
+
+    public int[][] getButacas() {
+        return butacas;
+    }
+
+    public void setButacas(int[][] butacas) {
+        this.butacas = butacas;
+    }
+
 }
