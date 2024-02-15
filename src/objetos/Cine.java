35079827860001cc86692a281c946710fba02619
CLAUDIA CANDELAS OVIEDO <claudiacandelas2005@gmail.com>
2024-02-15 19:44:13 +0100

    (feat): Se añaden getters y setters en la clase Cin[De

--- src/objetos/Cine.java
+++ src/objetos/Cine.java
@@ -1,4 +1,39 @@
 package objetos;
 
 public class Cine {
+
+    private String nombreCine;
+    private int aforo;
+    private int numSalas;
+
+    public Cine(String nombreCine, int aforo, int numSalas) {
+        this.nombreCine = nombreCine;
+        this.aforo = aforo;
+        this.numSalas = numSalas;
+    }
+
+    public String getNombreCine() {
+        return nombreCine;
+    }
+
+    public void setNombreCine(String nombreCine) {
+        this.nombreCine = nombreCine;
+    }
+
+    public int getAforo() {
+        return aforo;
+    }
+
+    public void setAforo(int aforo) {
+        this.aforo = aforo;
+    }
+
+    public int getNumSalas() {
+        return numSalas;
+    }
+
+    public void setNumSalas(int numSalas) {
+        this.numSalas = numSalas;
+    }
+
 }
