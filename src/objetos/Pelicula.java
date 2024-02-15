0bdd2538bbf6c51983915ac737f85c3ef9894367
CLAUDIA CANDELAS OVIEDO <claudiacandelas2005@gmail.com>
2024-02-15 19:23:06 +0100

    (feat): Se añaden getters y setters en la clase Pelicula

--- src/objetos/Pelicula.java
+++ src/objetos/Pelicula.java
@@ -1,4 +1,33 @@
 package objetos;
 
 public class Pelicula {
+
+    private String titulo;
+    private int duracion;
+
+    public Pelicula(String titulo, int duracion) {
+        this.titulo = titulo;
+        this.duracion = duracion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+
 }
